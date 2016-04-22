7749fa3ef5ad3e24d29ab59b9426b5f135696253
vgandhi <varun.gandhi@mathworks.com>
2016-04-22 16:40:26 -0400

    Added skills and about sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,52 @@
 $(function() {
-    //     $('.navbar-nav a').click(function() {
-    //         console.log('Scroll', this)
-    //         $('html, body').animate({
-    //             scrollTop: $($(this).attr('href')).offset().top
-    //         }, 500);
-    //         return false;
-    //     });
-    // Updating which nav bar item is active
-    $(".nav a").on("click", function() {
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-        collapseNavbar();
-    });
 
-    $(document).on("click touchend", function() {
-        collapseNavbar();
-    });
+    var $window = $(window);
+
+    // Page Navigation logic
+    var makeNavItemActive = function() {
+        $('html, body').animate({
+            scrollTop: $(navLink).offset().top
+        }, 500);
+    };
 
     var collapseNavbar = function() {
         $(".navbar-collapse.in").collapse('hide');
     };
+
+    // Updating which nav bar item is active
+    $(".nav a").on("click", function() {
+        $('html, body').animate({            
+            scrollTop: $('.' + $(this).attr('id')).offset().top
+        }, 500);
+        collapseNavbar();
+        console.log($(this).attr('id'));
+    });
+
+    // Collapse navbar on touch/click
+    $(document).on("click touchend", function() {
+        collapseNavbar();
+    });
+
+    // Update progress bars on scrolling into view
+    var isScrolledIntoView = function(elem) {
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = elem.offset().top;
+        var elemBottom = elemTop + elem.height();
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }; 
+    
+    var skills = [$("#javaSkill"), $("#cppSkill"), $("#mlSkill"), $("#jsSkill"), $("#htmlSkill"), $("#cssSkill")];
+    var skillVal = [80, 60, 100, 60, 50, 40];
+    $(document).scroll(function() {
+        skills.forEach(function(elem, index) {
+            // animate programming skill
+            var val = (isScrolledIntoView(elem)) ? skillVal[index] : 0;            
+            elem.css('width', val + '%').attr('aria-valuenow', val);
+        });
+    });
+    
     console.log("JS Ready");
 });
